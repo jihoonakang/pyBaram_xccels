036e207ec69a1128450b99368b22f89ab2281e5d
Jin Seok Park <jinseok.park@inha.ac.kr>
2023-02-15 16:43:16 +0900

    Version 0.2.5

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'pyBaram'
-copyright = '2022, Inha University'
+copyright = '2022-2023, Inha University'
 author = 'Jin Seok Park'
 
 # The full version, including alpha/beta/rc tags
